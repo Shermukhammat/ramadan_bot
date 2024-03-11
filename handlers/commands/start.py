369028a90a24ecfaf9a4b2577e3dacd9b8faa872
Shermukhammat <shermukhammadtemirov@gmail.com>
2024-03-11 12:44:05 +0500

    user regitaion added for start habdler

--- handlers/commands/start.py
+++ handlers/commands/start.py
@@ -1,8 +1,14 @@
-from loader import dp
+from loader import dp, db
 from aiogram.dispatcher import FSMContext
 from aiogram import types
 
 
 @dp.message_handler(commands = 'start', state = "*")
 async def start_command_handler(update : types.Message, state : FSMContext):
-    await update.answer("Assalom alykum")
+    if db.is_user(update.from_user.id):
+        await update.answer("You are user")
+        
+    else:
+        await update.answer(f"Assalomu alykum {update.from_user.first_name} men @ramazon2024_robot man. Men sizga iftorlik va saharlik vaxtlarni aytib bera olaman")
+        db.registir(id = update.from_user.id, name = update.from_user.first_name)
+        
